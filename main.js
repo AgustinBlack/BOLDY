3be06d177c430d36d414292fa80cbb575793fa72
Agustin Black <agustinblack04@gmail.com>
2023-04-14 10:57:24 -0300

    Creacion de cards y sus estilos, modificaciones en el html y css

--- main.js
+++ main.js
@@ -1,71 +1,75 @@
 const zapatillas = []
 
 class Zapatilla{
-    constructor(marca,modelo,precio,stock,id){
+    constructor(marca,modelo,precio,stock,id,image){
         this.marca = marca;
         this.modelo = modelo;
         this.precio = precio;
         this.stock = stock;
         this.id = id;
+        this.image = image;
     }
 }
 
-let adidas1 = new Zapatilla("Adidas",`Adidas All Star`,16000,5,1)
-let adidas2 = new Zapatilla("Adidas",`Adidas Forum Low`,14000,9,2)
-let adidas3 = new Zapatilla("Adidas",`Adidas Ozelia`,19000,4,3)
-let adidas4 = new Zapatilla("Adidas",`Adidas X9000L4`,21000,2,4)
+let adidas1 = new Zapatilla("Adidas",`Adidas All Star`,16000,5,1, './images/adidas-all-star.jpg')
+let adidas2 = new Zapatilla("Adidas",`Adidas Forum Mid GW2857`,14000,9,2, './images/Forum-Mid-GW2857.jpg')
+let adidas3 = new Zapatilla("Adidas",`Adidas Ozelia`,19000,4,3, './images/adidas-ozelia.jpg')
+let adidas4 = new Zapatilla("Adidas",`Adidas X9000L4`,21000,2,4, './images/adidas-x9000l4-blancas.jpg')
 
-let nike1 = new Zapatilla("Nike",`Nike Air Jordan 1`,17000,6,5)
-let nike2 = new Zapatilla("Nike",`Nike Air Max 97`,19000,2,6)
-let nike3= new Zapatilla("Nike",`Nike air Max Bolt`,21000,5,7)
-let nike4 = new Zapatilla("Nike",`Nike Air Force 1 High 07`,25000,8,8)
+let nike1 = new Zapatilla("Nike",`Nike Air Jordan 1`,17000,6,5, './images/air-jordan-1.jpg')
+let nike2 = new Zapatilla("Nike",`Nike Air Max 97`,19000,2,6, './images/air-max-97.jpg')
+let nike3= new Zapatilla("Nike",`Nike air Max Bolt`,21000,5,7, './images/air-max-bolt(1).jpg')
+let nike4 = new Zapatilla("Nike",`Nike Air Force 1 High 07`,25000,8,8, './images/air-max-force-1-hich-07.jpg')
 
-let puma1 = new Zapatilla("Puma",`Puma Slipstream`,13000,9,9)
-let puma2 = new Zapatilla("Puma",`Puma Trc Blaze`,16000,4.10)
-let puma3 = new Zapatilla("Puma",`Puma CA Pro Classic`,18000,6,11)
-let puma4 = new Zapatilla("Puma",`Puma Baquet Classic XXI`,17000,2,12)
+let puma1 = new Zapatilla("Puma",`Puma 19486201`,13000,9,9, './images/PUMA-19486201.jpg')
+let puma2 = new Zapatilla("Puma",`Puma Trc Blaze`,16000,4,10, './images/puma-trc-blaze.jpg')
+let puma3 = new Zapatilla("Puma",`Puma CA Pro Classic`,18000,6,11, './images/puma-ca-pro-classic.jpg')
+let puma4 = new Zapatilla("Puma",`Puma Baquet Classic XXI`,17000,2,12, './images/puma-basket-classic-xxi.jpg')
 
-let newBalance1 = new Zapatilla("New Balance",`Beige New Balance 550`,15000,2,13)
-let newBalance2 = new Zapatilla("New Balance",`Beige New Balance 530`,19000,7,14)
-let newBalance3 = new Zapatilla("New Balance",`Beige New Balance 990 V5`,14000,3,15)
-let newBalance4 = new Zapatilla("New Balance",`Beige New Balance 373`,20000,4,16)
+let newBalance1 = new Zapatilla("New Balance",`Beige New Balance 550`,15000,2,13, './images/Beige-New-Balance-550.jpg')
+let newBalance2 = new Zapatilla("New Balance",`Beige New Balance 530`,19000,7,14, './images/Beige-New-Balance-530.jpg')
+let newBalance3 = new Zapatilla("New Balance",`Beige New Balance 990 V5`,14000,3,15, './images/Beige-New-Balance-990-V5.jpg')
+let newBalance4 = new Zapatilla("New Balance",`Beige New Balance 373`,20000,4,16, './images/Beige-New-Balance-373.jpg')
 
 zapatillas.push(adidas1, adidas2, adidas3, adidas4, nike1, nike2, nike3, nike4, puma1, puma2, puma3, puma4, newBalance1, newBalance2, newBalance3, newBalance4)
 
 console.log(zapatillas)
 
-// // PROCESO PARA FILTRAR POR MARCA
-
-// let marcaElegida = prompt("Ingrese la marca que busca (Adidas, Nike, Puma o New Balance)")
-// const marcaFiltrada = zapatillas.filter((zapatilla) => zapatilla.marca === marcaElegida);
-// console.log(marcaFiltrada)
-
-// // FUNCION QUE SUMA EL PRECIO DE LAS ZAPATILLAS QUE ELEGIS
-
-// let primerPrecio = parseInt(prompt("Ingrese el valor de las primeras zapatillas"))
-// let segundoPrecio =parseInt(prompt("Ingrese el valor de las segundas zapatillas:"))
-// console.log("El valor de su compra es de" , primerPrecio + segundoPrecio)
-
-//AGREGANDO DE INFORMACION A LAS CARDS
-
-const contenedorCards = document.querySelector("#contenedor__cards");
+//CREACION DE CARDS
 
 function mostrarProductos(productos){
-
+    const contenedorCards = document.querySelector("#contenedor__cards");
+    contenedorCards.innerHTML = "";
     productos .forEach(p => {
-    const card = document.createElement("div");
-
-    card.innerHTML = `
-                        <div class="zapatillas__cards">
-                            <h3>${p.modelo}</h3>
-                            <h5>${p.marca}</h5>
-                            <p>${p.precio}</p>
-                            <p>${p.stock}</p>
-                            <button id="${p.id}">Compar producto</button>                         
-                        </div>
-                    `
-    contenedorCards.appendChild(card);
+        const card = document.createElement("div");
+        card.classList.add('card', 'col-xl-3', 'col-md-6', 'col-sm-12')
+        card.innerHTML = `
+                            <div class="card">
+                                <img src="${p.image}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">${p.modelo}</h5>
+                                    <p class="card-text">${p.marca}</p>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">$${p.precio}</li>
+                                    <li class="list-group-item">Cantidad: ${p.stock}</li>
+                                </ul>
+                                <div class="card-body">
+                                    <button id="boton${p.id}">Agregar al Carrito</button>
+                                </div>
+                            </div>`
+        contenedorCards.appendChild(card);
     });
 }
 
 mostrarProductos(zapatillas)
+
+//BUSCADOR
+
+let buscador = document.getElementById("buscador")
+buscador.addEventListener("input", filtrar)
+
+function filtrar(){
+    let zapatillasFiltradas = zapatillas.filter(zapatilla => zapatilla.marca.includes(buscador.value))
+    mostrarProductos(zapatillasFiltradas)
+}
