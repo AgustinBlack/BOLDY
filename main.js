ab333194f7884d014c21e509a964749c0f7e119d
Agustin Black <agustinblack04@gmail.com>
2023-05-04 19:27:20 -0300

    Cambios y agregado de fetch

--- main.js
+++ main.js
@@ -1,39 +1,14 @@
-const zapatillas = []
+const zapatillas = "./json/data.json"
+const contenedorProductos = document.getElementById("contenedorProductos")
 
-class Zapatilla{
-    constructor(marca,modelo,precio,stock,id,image){
-        this.marca = marca;
-        this.modelo = modelo;
-        this.precio = precio;
-        this.stock = stock;
-        this.id = id;
-        this.image = image;
-    }
-}
+fetch(zapatillas)
+  .then((respuesta) => respuesta.json)
+  .then((datos) => {
+    console.log(datos);
+    mostrarProductos(datos);
+  })
+  .catch((error) => console.log("Hubo un error"));
 
-let adidas1 = new Zapatilla("Adidas",`Adidas All Star`,16000,5,1, './images/adidas-all-star.jpg')
-let adidas2 = new Zapatilla("Adidas",`Adidas Forum`,14000,9,2, './images/Forum-Mid-GW2857.jpg')
-let adidas3 = new Zapatilla("Adidas",`Adidas Ozelia`,19000,4,3, './images/adidas-ozelia.jpg')
-let adidas4 = new Zapatilla("Adidas",`Adidas X9000L4`,21000,2,4, './images/adidas-x9000l4-blancas.jpg')
-
-let nike1 = new Zapatilla("Nike",`Nike Air Jordan 1`,17000,6,5, './images/air-jordan-1.jpg')
-let nike2 = new Zapatilla("Nike",`Nike Air Max 97`,19000,0,6, './images/air-max-97.jpg')
-let nike3= new Zapatilla("Nike",`Nike Air Max Bolt`,21000,2,7, './images/air-max-bolt(1).jpg')
-let nike4 = new Zapatilla("Nike",`Nike Air Force 1 High 07`,25000,8,8, './images/air-max-force-1-hich-07.jpg')
-
-let puma1 = new Zapatilla("Puma",`Puma 19486201`,13000,9,9, './images/PUMA-19486201.jpg')
-let puma2 = new Zapatilla("Puma",`Puma Trc Blaze`,16000,4,10, './images/puma-trc-blaze.jpg')
-let puma3 = new Zapatilla("Puma",`Puma CA Pro Classic`,18000,6,11, './images/puma-ca-pro-classic.jpg')
-let puma4 = new Zapatilla("Puma",`Puma Baquet Classic XXI`,17000,2,12, './images/puma-basket-classic-xxi.jpg')
-
-let newBalance1 = new Zapatilla("New Balance",`Beige New Balance 550`,15000,2,13, './images/Beige-New-Balance-550.jpg')
-let newBalance2 = new Zapatilla("New Balance",`Beige New Balance 530`,19000,7,14, './images/Beige-New-Balance-530.jpg')
-let newBalance3 = new Zapatilla("New Balance",`Beige New Balance 990 V5`,14000,0,15, './images/Beige-New-Balance-990-V5.jpg')
-let newBalance4 = new Zapatilla("New Balance",`Beige New Balance 373`,20000,4,16, './images/Beige-New-Balance-373.jpg')
-
-zapatillas.push(adidas1, adidas2, adidas3, adidas4, nike1, nike2, nike3, nike4, puma1, puma2, puma3, puma4, newBalance1, newBalance2, newBalance3, newBalance4)
-
-console.log(zapatillas)
 
 //BUSCADOR
 
@@ -50,23 +25,23 @@ function filtrar(){
 function mostrarProductos(arrayProductos) {
     let contenedor = document.getElementById("wrapper")
     contenedor.innerHTML = ""
-    arrayProductos.forEach(({ modelo, marca, image, precio, stock, id }) => {
+    arrayProductos.forEach((producto) => {
       let card = document.createElement("div")
       card.classList.add('card', 'col-xl-3', 'col-md-6', 'col-sm-12')
   
       card.innerHTML = `
         <div class="card">
-            <img src="${image}" class="card-img-top" alt="...">
+            <img src="${producto.image}" class="card-img-top" alt="...">
             <div class="card-body">      
-                <h5 class="tituloProducto">${modelo}</h5>
-                <p class="card-title">${marca}</p>
+                <h5 class="tituloProducto">${producto.modelo}</h5>
+                <p class="card-title">${producto.marca}</p>
             </div>
             <ul class="list-group list-group-flush">
-                <p class="list-group-item">Precio: $${precio}</p>
-                <p class="list-group-item">Quedan <span id=span${id}>${stock}</span> unidades</p>
+                <p class="list-group-item">Precio: $${producto.precio}</p>
+                <p class="list-group-item">Quedan <span id=span${producto.id}>${producto.stock}</span> unidades</p>
             </ul>
             <div class="card-body">
-                <button class="boton__card" id=${id}>Agregar al carrito</button>
+                <button class="boton__card" id=${producto.id}>Agregar al carrito</button>
             </div>
         </div>
       `
@@ -199,3 +174,8 @@ function compraRealizada() {
         onClick: function(){}
       }).showToast();
 }
+
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//   .then(respuesta => respuesta.json())
+//   .then(info => console.log(info))
+//   .catch(error => console.log("HUBO UN ERRORS"))
